85c6e7eed362bfc5cc353e866617040dd4bb1fc0
Salvador Rodríguez <tenajesus@hotmail.com>
2021-12-11 13:21:52 -0600

    Se habilita el módulo de consulta para solicitud

--- src/pages/Status.vue
+++ src/pages/Status.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="text-green text-center q-pa-md">
     <div class="row">
-            <div class="col-4">
-            </div>
-            <div class="col-4">
-              <label>Ingresa tu matricula</label>
-              <q-input bg-color="teal-1" outlined v-model="matriculauser" 
-              />
-              <br>
-              <q-btn
-        color="teal"
-        glossy
-        text-color="white"
-        icon-right="search"
-        label="Verificar Status"
-        @click="buscarRegistro(this.matriculauser)"
-      />
-            </div>
-            <div class="col-4">
-            </div>    
-      </div>   
-    <br>
+      <div class="col-4"></div>
+      <div class="col-4">
+        <label>Ingresa tu matricula</label>
+        <q-input bg-color="teal-1" outlined v-model="matriculauser" />
+        <br />
+        <q-btn
+          color="teal"
+          glossy
+          text-color="white"
+          icon-right="search"
+          label="Verificar Status"
+          @click="buscarRegistro(this.matriculauser)"
+        />
+      </div>
+      <div class="col-4"></div>
+    </div>
+    <br />
     <q-card class="my-card text-black">
       <q-card-section>
         <div class="text-h7">Status de tu solicitud</div>
@@ -31,27 +28,27 @@
           <div class="row">
             <div class="col-6">
               <label>Fecha de Captura</label>
-              <q-input bg-color="teal-1" outlined v-model="fecha_captura"/>
+              <q-input bg-color="teal-1" outlined disable v-model="fecha_captura" />
             </div>
             <div class="col-6">
               <label>Número de Folio</label>
-              <q-input bg-color="teal-1" outlined v-model="folio"/>
+              <q-input bg-color="teal-1" outlined disable v-model="folio" />
             </div>
           </div>
         </center>
-        <br>
+        <br />
         <div class="row">
-            <div class="col-12">
-              <label>Nombre</label>
-              <q-input bg-color="teal-1" outlined v-model="nombre"/>
-            </div>
+          <div class="col-12">
+            <label>Nombre</label>
+            <q-input bg-color="teal-1" outlined disable v-model="name" />
+          </div>
         </div>
-        <br>
+        <br />
         <div class="row">
-            <div class="col-12">
-              <label>Te informamos que tu solicitud está:</label>
-              <q-input bg-color="teal-1" outlined v-model="estatus"/>
-            </div>
+          <div class="col-12">
+            <label>Te informamos que tu solicitud está:</label>
+            <q-input bg-color="teal-1" outlined disable v-model="estatus" />
+          </div>
         </div>
       </q-card-section>
 
@@ -64,59 +61,59 @@
 import { defineComponent } from "vue";
 import axios from "axios";
 
-
 export default defineComponent({
   name: "Contacto",
   data() {
     return {
       instrucciones:
         "Por favor cargue los archivos solicitados para concluir con el proceso",
-      fecha_captura:'',
-      folio:'',
-      nombre:'',
-      estatus:'',
-      matriculauser:''
+      fecha_captura: "",
+      folio: "",
+      name: "",
+      estatus: "",
+      matriculauser: "",
     };
-  }, methods:{
+  },
+  methods: {
     buscarRegistro(matriculauser) {
-      
-      if(!this.matriculauser){
+      if (!this.matriculauser) {
         this.$q.notify({
-        message: "No se ha ingresado la matricula, o no se encontró tu registro",
-        color: "red",
-        position: "top-right",
-      });
+          message:
+            "No se ha ingresado la matricula, o no se encontró tu registro",
+          color: "red",
+          position: "top-right",
+        });
       } else {
         axios
-          .get("http://192.168.0.102/stpsback/public/index.php/api/solicitudes/"+matriculauser)
+          .get(
+            "http://localhost/stpsback/public/index.php/api/solicitudes/" +
+              matriculauser
+          )
           .then((res) => {
+            const data = res.data;
+            console.log(data);
 
-              const data = res.data;
-              console.log(data.length);
-              
-              // this.nombre = data[0].nombre;
-              
-              if(data.length > 0){
+           if (data.length > 0) {
               this.folio = data[0].id;
               this.fecha_captura = data[0].fechacaptura;
+              this.name = data[0].nombre;
+              this.estatus = data[0].status_solicitud;
               this.$q.notify({
                 message: "Se ha localizado " + data.length + " registro",
                 color: "teal",
                 position: "top-right",
-                icon:"done"
+                icon: "done",
               });
-              console.log(data.length);
-              } else {
-                this.$q.notify({
-              message: "No se ha encontrado tu registro",
-              color: "red",
-              position: "top-right",
-            });
-              }
+            } else {
+              this.$q.notify({
+                message: "No se ha encontrado tu registro",
+                color: "red",
+                position: "top-right",
+              });
+            }
           });
       }
-      
     },
-  }
+  },
 });
 </script>
